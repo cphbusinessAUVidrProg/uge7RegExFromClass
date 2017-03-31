c6470bdbc0b134ef1473a5a48d441a3f2a8ed7a1
Kasper Osterbye(win) <koe@cphbusiness.dk>
2017-03-31 07:49:26 +0200

    Added some comments to the code

--- RegExForm/RegExForm.cs
+++ RegExForm/RegExForm.cs
@@ -18,6 +18,9 @@ namespace Sofus
             InitializeComponent();
         }
         
+        // This method is called when we type in the test text window
+        // You can see this in line 52 of the RegExForm.Designer.cs file
+        // (You need to open that part of the source code by the small "+" in line 23) 
         private void testText_TextChanged(object sender, EventArgs e)
         {
             string ptn = regExText.Text;
@@ -33,13 +36,13 @@ namespace Sofus
         }
 
         private void buttonON_Click(object sender, EventArgs e)
-        {
-            regExText.TextChanged += this.testText_TextChanged;
+        {   // Add the testText_TextChanged method as observer on the regExText textbox
+            regExText.TextChanged += this.testText_TextChanged; // += adds event handler
         }
 
         private void buttonOFF_Click(object sender, EventArgs e)
-        {
-            regExText.TextChanged -= this.testText_TextChanged;
+        {   // remove the testText_TextChanged method as observer on the regExText textbox
+            regExText.TextChanged -= this.testText_TextChanged; // -= removes it
         }
     }
 }
